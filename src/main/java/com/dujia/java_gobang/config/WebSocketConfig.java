4c49a310489f7ecb5eea1ebcc1544f8cced46b0d
exclusive-memories <2917794747@qq.com>
2025-06-22 19:49:41 +0800

    feat(websocket): Configure WebSocket for real-time game communication

---
+++ src/main/java/com/dujia/java_gobang/config/WebSocketConfig.java
@@ -0,0 +1,30 @@
+package com.dujia.java_gobang.config;
+
+import com.dujia.java_gobang.api.GameAPI;
+import com.dujia.java_gobang.api.MatchAPI;
+import com.dujia.java_gobang.api.TestAPI;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Autowired
+    private TestAPI testAPI;
+    @Autowired
+    private MatchAPI matchAPI;
+    @Autowired
+    private GameAPI gameAPI;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(testAPI, "/test");
+        registry.addHandler(matchAPI, "/findMatch").addInterceptors(new HttpSessionHandshakeInterceptor());
+        registry.addHandler(gameAPI, "/game").addInterceptors(new HttpSessionHandshakeInterceptor());
+    }
+}
